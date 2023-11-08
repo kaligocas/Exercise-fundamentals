4513ab229707890dd16cb5703a9170ddc6835a88
katherine Gómez <kaligocas1990@gmail.com>
2023-11-08 15:16:27 +0100

    firts commit

---
+++ src/fundamentals-ex/stringExercises.js
@@ -0,0 +1,43 @@
+export function returnHello() {
+    /* Completa la function */
+
+}
+
+export function returnAString() {
+    /* Completa la function */
+
+}
+
+export function concatenateStrings() {
+    const firstWord = 'I';
+    const secondWord = 'Love';
+    /* Completa la function */
+
+}
+
+export function giveOneLetter() {
+    const dog = 'dog';
+    /* Completa la function */
+}
+
+export function giveTheIndexOfWord() {
+    const wordToSearch = 'Butterfly';
+    const phrase = "Once upon a time!!!";
+    /* Completa la function */
+}
+
+export function replaceFishWord() {
+    let phrase = 'Give a Man a Fish';
+    /* Completa la function */
+}
+
+export function giveAllLettersInUppercase() {
+    let phrase = 'Ride Him, Cowboy!';
+    /* Completa la function */
+}
+
+export function removeWhiteSpaces() {
+    let phrase = '    Hands Down    ';
+    /* Completa la function */
+
+}
